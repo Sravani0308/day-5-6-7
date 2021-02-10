d45e71974885a10cdaa8bb345c1a4f1e7bec47f3
Sravani0308 <78408113+Sravani0308@users.noreply.github.com>
2021-02-10 05:49:42 -0800

    Add files via upload

---
+++ While.java
@@ -0,0 +1,20 @@
+package day7;
+
+public class While {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		int num=5;
+		while(num>0)
+		{
+			System.out.println("number= "+num);
+			num--;
+			
+		}
+		
+	}
+
+}
